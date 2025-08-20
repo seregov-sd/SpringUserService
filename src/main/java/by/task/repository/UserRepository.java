6169122a22b8a191270d5e85fb3c0face4442a34
Sergey Seregov <seregov.sd@yandex.by>
2025-08-21 01:23:26 +0300

    feat: add UserRepository with Spring Data JPA

---
+++ src/main/java/by/task/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package by.task.repository;
+
+import by.task.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
