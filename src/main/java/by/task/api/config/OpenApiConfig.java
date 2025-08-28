c1c4de56ef7d2336a4994fd39021c14126b0b425
Sergey Seregov <seregov.sd@yandex.by>
2025-08-29 01:15:38 +0300

    feat: add Swagger OpenAPI configuration

---
+++ src/main/java/by/task/api/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package by.task.api.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("User Management API")
+                        .version("1.0.0")
+                        .description("API for managing users with HATEOAS support")
+                        .contact(new Contact()
+                                .name("API Support")
+                                .email("api-support@task.by")));
+    }
+}
